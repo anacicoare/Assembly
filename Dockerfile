547152de7d171de004316b23e7f9297a6ff6d5c3
cristian-vijelie <cristianvijelie@gmail.com>
2022-05-15 19:06:22 +0300

    Add the timegate bonus task

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM jokeswar/base-ctl
 
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update -yqq && apt-get install -yqq gcc-multilib nasm bc qemu-user
+RUN apt-get update -yqq && apt-get install -yqq gcc-multilib nasm bc qemu-user gdb
 
 COPY ./checker ${CHECKER_DATA_DIRECTORY}
---
+++ checker/bonus_timegate/checker/check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+TASK_SCORE=5
+TOTAL=0
+ZERO_FOUND=0
+NONZERO_FOUND=0
+
+echo "====================== Timegate bonus task  ======================="
+
+for i in 1 2 3 4 5; do
+	OUTPUT=$(./checker)
+	if [[ $OUTPUT == "0" ]]; then 
+		ZERO_FOUND="1"
+		break
+	fi	
+done
+for i in 1 2 3 4 5; do
+	OUTPUT=$(gdb ./checker -x gdb_commands | tail -n 2 | head -n 1)
+	if [[ $OUTPUT != "0" ]]; then
+		NONZERO_FOUND="1"
+		break
+	fi
+done
+if [[ $ZERO_FOUND == "1" ]]; then
+	echo "Error: You didn't generate a random number, but you should have"
+fi
+if [[ $NONZERO_FOUND == "1" ]]; then
+	echo "Error: You generated a random number, but you shouldn't have"
+fi
+if [[ $ZERO_FOUND == "0" ]] && [[ $NONZERO_FOUND == "0" ]]; then
+	TOTAL=$TASK_SCORE
+fi
+
+echo
+printf "Total score:				%3.2fp/%3.2fp\n" ${TOTAL} ${TASK_SCORE} | tr ',' '.'
+
+make clean > /dev/null 2>&1
+
+echo "bonus_timegate:${TOTAL}" >> ../../.results
