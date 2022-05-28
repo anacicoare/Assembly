abc7f3e5cda8c3d6f5a47e8dd28f1abaf615cbef
cristian-vijelie <cristianvijelie@gmail.com>
2022-05-28 18:43:02 +0300

    Fix bug in task-4

--- checker/task-4/checker/check.sh
+++ checker/task-4/checker/check.sh
@@ -127,7 +127,7 @@ else
 fi
 
 CACHE_LINE_REF=$(cat /sys/devices/system/cpu/cpu0/cache/index2/coherency_line_size)
-CACHE_SIZE_REF=$(cat /sys/devices/system/cpu/cpu0/cache/index2/size) | tr 'K' ''
+CACHE_SIZE_REF=$(cat /sys/devices/system/cpu/cpu0/cache/index2/size) | tr 'K' '\0'
 
 if [ "$CPU" == "GenuineIntel" ]; then
     CACHE=$(cat $OUTPUT_INTEL | tail -n 1)
